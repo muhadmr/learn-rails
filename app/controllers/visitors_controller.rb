96825d8db1ce1f0afc3dd300cb99c95e63318a28
Muhammad Mohd Rais <muhadmr@gmail.com>
2014-06-18 17:28:07 +0800

    front end framework - zurb'

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,12 +2,6 @@ class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
    # render 'visitors/new', :layout => 'special'
-   	flash.now[:notice] = 'Welcome'
-   	flash.now[:alert] = 'My birthday is sooon'
-   #flash[:anything] = 'Try je ni'
 
-   	flash.each do |key, value|
-   	 	puts '<div class="' + key +'">' + value + '</div>' 
-   	end
    end
 end
